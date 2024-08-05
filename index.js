69902fd85a622ce8f38922947e35fc9b84013d6a
Gerson Mayer <g.mayerduran@gmail.com>
2024-08-04 23:47:28 -0500

    adds print format

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { taxExckudedProducts, taxRates, inputs } from "./data.js";
+import { taxRates, inputs } from "./data.js";
 
 
 const calculateBasicTax = (product)=>{
@@ -23,15 +23,35 @@ const calculateImportedTax = (product) =>{
     }
 } 
 
-const getTaxes = (products) =>{
+const getTaxesPerProducts = (products) =>{
     return products.map((product) =>{
         const basicTax = calculateBasicTax(product)
         const importationTax = calculateImportedTax(basicTax)
+        importationTax["totalTax"]= importationTax.basicTax + importationTax.importationTax
+        importationTax["itemTotal"]= (importationTax.totalTax + importationTax.price)*importationTax.quantity 
         return importationTax 
     })
-    
 }
 
-inputs.forEach((products)=>{
-    console.log(getTaxes(products))
+const getSalesTaxes = (products) =>{
+    const saleTax = products.reduce((accumulator, currentProduct)=>{
+       return  accumulator += currentProduct.totalTax
+    }, 0)
+    return saleTax
+}
+
+
+const results = inputs.map((sale)=>{
+    const taxedProducts = getTaxesPerProducts(sale)
+    const saleTaxes = getSalesTaxes(taxedProducts)
+    const grandTotal =  taxedProducts.reduce((accumulator, product) => accumulator += product.itemTotal, 0)
+    const result = { items: taxedProducts, saleTaxes, grandTotal}
+    return result
 })
+
+
+results.forEach((result)=>{
+    result.items.forEach((item) => console.log(`${item.quantity} ${item.item} ${item.itemTotal}`))
+    console.log(`Sales taxes: ${result.saleTaxes}`)
+    console.log(`Total: ${result.grandTotal}`)
+} )
